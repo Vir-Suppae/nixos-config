cd3c596b5b0d9721641b92f9a5b6953e4083778b
Vir-Suppae <vir.suppae@gmail.com>
2026-08-24 15:57:47 -0500

    fixed an issue in unipick

--- packages/unipicker/package.nix
+++ packages/unipicker/package.nix
@@ -12,6 +12,6 @@ writeShellApplication {
   text = ''
     fuzzel -d -w 120 --font "monospace:size=9" < ${./unipicker-database.txt} |
       python3.14 -c "import sys; sys.stdout.write(chr(int(sys.stdin.read().split('\t')[2].strip(), 16)))" |
-      wl-copy --type text/plain;charset=utf-8
+      wl-copy --type 'text/plain;charset=utf-8'
   '';
 }
