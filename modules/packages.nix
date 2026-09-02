1668da397225f40be52e1f44cc33d8ea1ae88ace
Vir-Suppae <vir.suppae@gmail.com>
2026-09-02 13:26:13 -0500

    changed startup shell command in kitty

--- modules/packages.nix
+++ modules/packages.nix
@@ -71,7 +71,7 @@ in
         tab_bar_style = "powerline";
         tab_bar_min_tabs = 1;
         tab_activity_symbol = "⌬";
-        shell = "/run/current-system/sw/bin/fish";
+        shell = "/run/current-system/sw/bin/fish -C fastfetch";
         editor = "/run/current-system/sw/bin/hx";
       };
       themeFile = "Carbonfox";
